2c61cbd8cebfc3a153eb9eaecf66111735dff2aa
Brimarx <brim.carrera@gmail.com>
2015-11-27 01:15:24 -0600

    Deberia de publicar

--- app/src/main/java/yanomas/android/gt/myapplication/vista/MainActivity.java
+++ app/src/main/java/yanomas/android/gt/myapplication/vista/MainActivity.java
@@ -6,16 +6,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import yanomas.android.gt.myapplication.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText etNicknameEP, etTituloEP,etContenidoEP;
+    private EditText etNicknameEP, etTituloEP, etContenidoEP;
     private RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +45,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        Publicacion publicacion = new Publicacion(
+                etNicknameEP.getText().toString(),
+                etTituloEP.getText().toString(),
+                etContenidoEP.getText().toString());
+        crearPublicacion(publicacion);
+    }
 
+    private void crearPublicacion(final Publicacion publicacion) {
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                Parametros.URL_JSON,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("nickname", publicacion.getNickname());
+                params.put("titulo", publicacion.getTitulo());
+                params.put("coment", publicacion.getComentario());
+                params.put("imagen","1");
+                return params;
+            }
+        };
+        requestQueue.add(request);
     }
 }
---
+++ app/src/main/java/yanomas/android/gt/myapplication/vista/Parametros.java
@@ -0,0 +1,8 @@
+package yanomas.android.gt.myapplication.vista;
+
+/**
+ * Created by David on 27/11/2015.
+ */
+public class Parametros {
+    protected static final String URL_JSON = "http://cinemitad.com/yanomas/json.php";
+}
