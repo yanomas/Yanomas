b8e58763473a232a18cececa8af3727940e7c22e
Brimarx <brim.carrera@gmail.com>
2015-11-27 00:17:53 -0600

    Se borro metodos innecesarios

--- app/src/main/java/yanomas/android/gt/myapplication/vista/MainActivity.java
+++ app/src/main/java/yanomas/android/gt/myapplication/vista/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         iniciaVars();
+        //hola
 
     }
 
